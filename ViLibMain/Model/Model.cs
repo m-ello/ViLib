13df1c9477cbb4957b9b38a770aaa3371858b671
sanya <126627611+m-ello@users.noreply.github.com>
2025-05-27 19:35:32 +0300

    modified borrowRecord data parser

--- ViLibMain/Model/Model.cs
+++ ViLibMain/Model/Model.cs
@@ -554,13 +554,34 @@ namespace ViLib
                 return null;
             }
 
+            var format = "dd.MM.yyyy HH:mm:ss";
+            var culture = CultureInfo.InvariantCulture;
+
+            if (!DateTime.TryParseExact(toks[3], format, culture, DateTimeStyles.None, out DateTime borrowDate))
+            {
+                return null;
+            }
+
+            DateTime? returnDate = null;
+            if (!string.IsNullOrEmpty(toks[4]))
+            {
+                if (DateTime.TryParseExact(toks[4], format, culture, DateTimeStyles.None, out DateTime parsedReturnDate))
+                {
+                    returnDate = parsedReturnDate;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
             return new BorrowRecord
             {
                 Id = int.Parse(toks[0]),
                 Book = book,
                 Client = client,
-                BorrowDate = DateTime.Parse(toks[3]),
-                ReturnDate = string.IsNullOrEmpty(toks[4]) ? null : (DateTime?)DateTime.Parse(toks[4])
+                BorrowDate = borrowDate,
+                ReturnDate = returnDate
             };
         }
     }
